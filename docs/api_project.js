59ad5dd899a34037cc951d373f958b7394d08c06
Alpha Olomi <alphaolomi@gmail.com>
2020-04-28 23:18:08 +0300

    feat(docs): add api docs

---
+++ docs/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Nodejs-App API",
+  "version": "1.0.0",
+  "description": "Node-APP with Nginx API documentation",
+  "title": "Node-App API Documentation",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-04-28T20:17:16.069Z",
+    "url": "http://apidocjs.com",
+    "version": "0.22.0"
+  }
+});
